dbd3054661ddc84668f756bd4fa5b57af8de91bb
mwip <matthias.weigand@protonmail.com>
2019-04-24 14:44:03 +0200

    removed old code from dbinteraction

--- recpp/include/dbinteraction.cpp
+++ recpp/include/dbinteraction.cpp
@@ -1,8 +1,5 @@
 #include "dbinteraction.h"
 #include "cookbook.h"
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <sqlite3.h>
 #include <QtSql>
 #include <QDebug>
 #include <vector>
@@ -23,68 +20,10 @@ dbInteraction::~dbInteraction(){
 
 }
 
-//bool dbInteraction::dbAccessible(){
-
-//    if (this->rc){
-//        return false;
-//    } else {
-//        return true;
-//    }
-//}
-
-//std::vector<QString> dbInteraction::listCookbooks(QString dbname){
-//    std::vector<QString> cbNames;
-//    const char *sql;
-
-//    this->rc = sqlite3_open(dbFile, &db);
-//    sql = "SELECT NAME FROM COOKBOOKS";
-//    sqlite3_stmt *stmt;
-
-//    if (this->rc){
-//        qDebug() << "Error opening database while listing Cookbooks";
-//    } else {
-//        this->rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
-//        if (this->rc != SQLITE_OK){
-//            qDebug() << "Error opening data base with statement while listing Cookbooks";
-//        } else {
-//            while ((this->rc = sqlite3_step(stmt)) == SQLITE_ROW){
-//                const unsigned char *name = sqlite3_column_text(stmt, 0);
-//                QString qsName = getQStringFromUnsignedChar(name);
-//                cbNames.push_back(qsName);
-//            }
-//        }
-//    }
-
-//    return cbNames;
-//}
-
-//TODO
-//Cookbook dbInteraction::loadCookbook(QString name){
-//    Cookbook *cb = new Cookbook;
-//    QString descr = "";
-//    QString comm = "";
-//    QString recipeIDs = "";
-//    QString fileName = "";
-//    int nOfRec = 0;
-
-////    if (this->dbAccessible()){
-////        this->sql = "SELECT * FROM RECIPES;";
-////        return parseSqlToCookbook(this->sql);
-////    }
-
-//    cb->setName(name);
-//    cb->setDescription(descr);
-//    cb->setComment(comm);
-
-//    return *cb;
-//}
-
-// TODO
-//Recipe dbInteraction::loadRecipe(const char *dbFile, int recipeId){
-
-//}
-
 void dbInteraction::initializeDatabase(){
+    // initialize a database from scratch
+    // creates required tables
+
     // open the data base
     if(this->db.open()){
         qDebug() << "Data base opened successfully";
@@ -130,6 +69,8 @@ void dbInteraction::initializeDatabase(){
 }
 
 void dbInteraction::createDummyRecipe(){
+    // creates a dummy recipe within the cookbook
+
     // open the data base
     if(this->db.open()){
         qDebug() << "Data base opened successfully";
